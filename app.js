fe9256b5dba52563f01817a967ee479874d4c454
Darkhan <94224520+Xant1@users.noreply.github.com>
2022-12-15 19:54:02 +0500

    read more feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const urlencodedParser = express.urlencoded({ extended: false });
 const PORT = 7777;
+const Post = require('./models/model');
 //import routes
 const indexRouter = require('./routes/index.router');
 const createRouter = require('./routes/create.router');
@@ -22,3 +23,12 @@ app.post('/create', urlencodedParser, newPostRouter);
 app.post('/delete/:id', deleteRouter);
 app.get('/edit/:id', editRouter);
 app.post('/edit', urlencodedParser, editRouter);
+
+app.get('/post/:id', function (req, res) {
+  const post = Post.findOne({ where: { id: req.params.id } })
+    .then((data) => {
+      if (!data) res.redirect('/');
+      res.render('show.hbs', { post: data });
+    })
+    .catch((err) => console.log(err));
+});
